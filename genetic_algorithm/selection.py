8fd51d59f33bfb1d9ab290bc772e0685e01eaeb0
Grzegorz Piasny <106985215+GP233b@users.noreply.github.com>
2025-03-28 19:46:40 +0100

    rozbudowa selection

--- genetic_algorithm/selection.py
+++ genetic_algorithm/selection.py
@@ -1,4 +1,15 @@
 import numpy as np
+
+def elitist_selection(pop, scores, num_best=1):
+    best_indices = np.argsort(scores)[:num_best]
+    return pop[best_indices]
+
+def roulette_wheel_selection(pop, scores):
+    fitness = 1 / (scores + 1e-6)  
+    probabilities = fitness / np.sum(fitness)
+    selected_idx = np.random.choice(len(pop), p=probabilities)
+    return pop[selected_idx]
+
 def tournament_selection(pop, scores, k=3):
     selected = np.random.choice(len(pop), k, replace=False)
     return pop[min(selected, key=lambda i: scores[i])]

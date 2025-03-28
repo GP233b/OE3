80ec9d9bf781843f7b74ec28a4ff78407d9bb570
JakubPedryc16 <jakubpedryc@gmail.com>
2025-03-28 19:42:11 +0100

    Update on mutation and crossover functions

--- genetic_algorithm/crossover.py
+++ genetic_algorithm/crossover.py
@@ -1,4 +1,27 @@
 import numpy as np
+
 def one_point_crossover(parent1, parent2):
     point = np.random.randint(1, len(parent1))
-    return np.concatenate((parent1[:point], parent2[point:])), np.concatenate((parent2[:point], parent1[point:]))
+    child1 = np.concatenate((parent1[:point], parent2[point:]))
+    child2 = np.concatenate((parent2[:point], parent1[point:]))
+    return child1, child2
+
+def two_point_crossover(parent1, parent2):
+    point1, point2 = sorted(np.random.randint(1, len(parent1), 2))
+    child1 = np.concatenate((parent1[:point1], parent2[point1:point2], parent1[point2:]))
+    child2 = np.concatenate((parent2[:point1], parent1[point1:point2], parent2[point2:]))
+    return child1, child2
+
+def uniform_crossover(parent1, parent2):
+    mask = np.random.randint(0, 2, len(parent1))
+    child1 = np.where(mask, parent1, parent2)
+    child2 = np.where(mask, parent2, parent1)
+    return child1, child2
+
+def granular_crossover(parent1, parent2, grain_size=2):
+    size = len(parent1)
+    child1, child2 = parent1.copy(), parent2.copy()
+    for i in range(0, size, grain_size):
+        if np.random.rand() < 0.5:
+            child1[i:i+grain_size], child2[i:i+grain_size] = parent2[i:i+grain_size], parent1[i:i+grain_size]
+    return child1, child2
--- genetic_algorithm/mutation.py
+++ genetic_algorithm/mutation.py
@@ -3,6 +3,23 @@ import sys
 sys.path.append('../')
 from config import X_MIN, X_MAX
 
+def boundary_mutation(individual, rate):
+    if np.random.rand() < rate:
+        idx = np.random.randint(len(individual))
+        individual[idx] = X_MIN if np.random.rand() < 0.5 else X_MAX
+    return individual
+
+def single_point_mutation(individual, rate):
+    if np.random.rand() < rate:
+        idx = np.random.randint(len(individual))
+        individual[idx] = np.random.uniform(X_MIN, X_MAX)
+    return individual
+
+def two_point_mutation(individual, rate):
+    if np.random.rand() < rate:
+        idx1, idx2 = np.random.choice(len(individual), 2, replace=False)
+        individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
+    return individual
 
 def gaussian_mutation(individual, rate):
     if np.random.rand() < rate:
